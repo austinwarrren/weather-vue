2a6285a9294c43939f7fcef3272221151ab247ce
Austin Warren <austin.warren@nwciowa.edu>
2019-03-23 13:11:08 -0500

    Created WeatherInfo.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <CityNameForm />
+    <WeatherInfo :cityName="cityName" />
+    <CityNameForm @submit="displayCity" />
   </div>
 </template>
 
 <script>
 import CityNameForm from './components/CityNameForm'
+import WeatherInfo from './components/WeatherInfo'
 
 export default {
   name: 'App', 
   components: {
-    CityNameForm
+    CityNameForm,
+    WeatherInfo
+  },
+  data() {
+    return {
+      cityName: ''
+    };
+  },
+  methods: {
+    displayCity(cityName) {
+      this.cityName = cityName;
+    }
   } 
 }
 </script>
