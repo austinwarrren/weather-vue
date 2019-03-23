d26def36dc35911c1046a6e2f822d3b4cfc40edb
Austin Warren <austin.warren@nwciowa.edu>
2019-03-23 17:45:52 -0500

    Modified WeatherData.spec.js to use a mocked HTTP request

--- tests/unit/WeatherData.spec.js
+++ tests/unit/WeatherData.spec.js
@@ -1,11 +1,24 @@
 import { expect } from 'chai';
 import WeatherData from '../../src/services/api/WeatherData.js'
+import nock from 'nock';
+import WeatherDataResponse from './mocks/weatherDataResponse.js';
 
 describe('WeatherData', () => {
-    it('returns JSON', () => {
+    beforeEach(() => {
+        nock('http://api.openweathermap.org')
+            .defaultReplyHeaders({'access-control-allow-origin': '*'})
+            .get('/data/2.5/weather')
+            .query({
+                q: 'Chicago',
+                APPID: process.env.VUE_APP_OPEN_WEATHER_MAP_KEY
+            })
+            .reply(200, WeatherDataResponse.currentWeatherData);
+    })
+    it('returns correct information', () => {
         return WeatherData.getCurrentWeatherData('Chicago')
             .then(response => {
-                expect(response).to.be.a('object');
+                //expect(response).to.be.a('object');
+                expect(response.name).to.equal('Chicago')
             });
     })
 })
